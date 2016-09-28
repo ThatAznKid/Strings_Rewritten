5ad559c372ebf40831659ee58c379834f109da7a
ThatAznKid <jchen47@stuy.edu>
2016-09-28 11:33:55 -0400

    first test

---
+++ stringy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int mystrlen (char s[]) {
+  int i = 0;
+  char *cp = s;
+  while (*cp) {
+    i++;
+    cp++;
+  }
+  return i;  
+}
+
+int main () {
+
+  char s1 = "Hello!\0";
+  printf ("Length of s1: %d\n:" , mystrlen (s1));
+  return 0;
+
+} 
